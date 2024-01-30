bd87d09d6422922bc0aad9eccca4d465fb1e0a8f
Pawel Balawender <pbalawender101@gmail.com>
2024-01-30 16:31:34 +0100

    dodaj dobry lcp

--- txt/zad2.cpp
+++ txt/zad2.cpp
@@ -76,139 +76,7 @@ using ull = unsigned long long int;
 // return from uniform distribution on [l, r]
 int randint(int l, int r) { uniform_int_distribution<int> dist (l, r); return dist(mt); }
 
-
-/*
- * Opis: O(n \log n) lub O(n \alpha),
- * 	Dla słowa \texttt{abaab\#} (hash jest aby to zawsze liście były stanami kończącymi)
- * 	stworzy \texttt{sons[0]=\{(\#,10),(a,4),(b,8)\}, sons[4]=\{(a,5),(b,6)\}, sons[6]=\{(\#,7),(a,2)\}, sons[8]=\{(\#,9),(a,3)\}},
- * 	reszta sons'ów pusta, \texttt{slink[6]=8} i reszta slink'ów $0$
- * 	(gdzie slink jest zdefiniowany dla nie-liści jako wierzchołek zawierający ten suffix bez ostatniej literki),
- * 	\texttt{up\_edge\_range[2]=up\_edge\_range[3]=(2,5), up\_edge\_range[5]=(3,5)} i reszta jednoliterowa.
- * 	Wierzchołek 1 oraz suffix wierzchołków jest roboczy.
- * 	Zachodzi \texttt{up\_edge\_range[0]=(-1,-1), parent[0]=0, slink[0]=1}.
- */
-struct SuffixTree {
-	const int n;
-	const vector<int> &_in;
-	vector<map<int, int>> sons;
-	vector<pair<int, int>> up_edge_range;
-	vector<int> parent, slink;
-	int tv = 0, tp = 0, ts = 2, la = 0;
-  // number of sentinels of given type (1, 2, 3) in subtree
-  vector<vector<int>> num_sentinels_below;
-  vector<size_t> sentinel_pos;
-  vector<unsigned long long int > sums;
-
-  void set_num_sentinels(int i) {
-    if (sons[i].empty()) {
-      int from = up_edge_range[i].first;
-
-      int n_sent = 1;
-      assert (from != -1);
-      if (from <= sentinel_pos[1]) { n_sent++; }
-      if (from <= sentinel_pos[0]) { n_sent++; }
-      num_sentinels_below[i][n_sent] = 1;
-      print("set num sentinels:", i, "= ");
-      println(num_sentinels_below[i]);
-      return;
-    } 
-    
-    for (auto [k, child]: sons[i]) {
-      set_num_sentinels(child);
-    }
-
-    for (int n_sent: {1, 2, 3}) {
-      for (auto [k, child]: sons[i]) {
-        num_sentinels_below[i][n_sent]+=num_sentinels_below[child][n_sent];
-      }
-    }
-    print("set num sentinels:", i, "= ");
-    println(num_sentinels_below[i]);
-   
-    sums[i] = num_sentinels_below[i][1] * num_sentinels_below[i][2] * num_sentinels_below[i][3];
-    for (auto [k, child]: sons[i]) {
-      sums[i] = (sums[i] + sums[child]) % 1000000007;
-    }
-    println("sums[", i, "] = ", sums[i]);
-    //sums[i] %= 1000000007;
-  }
-
-  void solve() {
-    set_num_sentinels(0);
-    if (sons[0].empty()) { return; }
-
-    println("sons:", sons);
-    println("sums:", sums);
-    int result = 0;
-    // length 1
-    deque<int> todo;
-    for (auto [k, child]: sons[0]) {
-      todo.push_back(child);
-      result = (result + sums[child]) % 1000000007;
-    }
-    print(result);
-
-    // lengths >= 2
-    while (!todo.empty()) {
-      deque<int> new_todo;
-      int layer_result = 0;
-      while(!todo.empty()) {
-        auto node = todo.front();
-        todo.pop_front();
-        layer_result = (layer_result + sums[node]) % 1000000007;
-        for (auto [k, child]: sons[node]) { new_todo.push_back(child); }
-      }
-
-      print("", layer_result);
-      todo = new_todo;
-    }
-    println("");
-  }
-
-	void ukkadd(int c) {
-		auto &lr = up_edge_range;
-suff:
-		if (lr[tv].second < tp) {
-			if (sons[tv].find(c) == sons[tv].end()) {
-				sons[tv][c] = ts; lr[ts].first = la; parent[ts++] = tv;
-				tv = slink[tv]; tp = lr[tv].second + 1; goto suff;
-			}
-			tv = sons[tv][c]; tp = lr[tv].first;
-		}
-		if (tp == -1 || c == _in[tp])
-			tp++;
-		else {
-			lr[ts + 1].first = la; parent[ts + 1] = ts;
-			lr[ts].first = lr[tv].first; lr[ts].second = tp - 1;
-			parent[ts] = parent[tv]; sons[ts][c] = ts + 1; sons[ts][_in[tp]] = tv;
-			lr[tv].first = tp; parent[tv] = ts;
-			sons[parent[ts]][_in[lr[ts].first]] = ts; ts += 2;
-			tv = slink[parent[ts - 2]]; tp = lr[ts - 2].first;
-			while (tp <= lr[ts - 2].second) {
-				tv = sons[tv][_in[tp]]; tp += lr[tv].second - lr[tv].first + 1;
-			}
-			if (tp == lr[ts - 2].second + 1)
-				slink[ts - 2] = tv;
-			else
-				slink[ts - 2] = ts;
-			tp = lr[tv].second - (tp - lr[ts-2].second) + 2; goto suff;
-		}
-	}
-	// Remember to append string with a hash.
-	SuffixTree(const vector<int> &in, int alpha, vector<size_t> sentinels)
-		: n(int(in.size())), _in(in), sons(2 * n + 1), sentinel_pos(sentinels), 
-		up_edge_range(2 * n + 1, pair(0, n - 1)), parent(2 * n + 1), slink(2 * n + 1), num_sentinels_below(2 * n + 1, vector<int>(4)), sums(2 * n + 1) {
-		up_edge_range[0] = up_edge_range[1] = {-1, -1};
-		slink[0] = 1;
-		// When changing map to vector, fill sons exactly here with -1 and replace if in ukkadd with sons[tv][c] == -1.
-		REP(ch, alpha)
-			sons[1][ch] = 0;
-		for(; la < n; ++la)
-			ukkadd(in[la]);
-	}
-};
-
-vector<size_t> get_suffix_array(const string& s) {
+vector<size_t> get_suffix_array_brut(const string& s) {
   vector<pair<string, size_t>> suffixes;
   for (size_t i=0; i < s.size(); ++i) {
     suffixes.push_back({s.substr(i), i});
@@ -272,7 +140,12 @@ vector<int> get_suffix_array2(const string& s) {
   return sorted_shifts;
 }
 
-vector<size_t> get_lcp_array(const string& s, const vector<size_t>& sa) {
+
+vector<int> get_suffix_array(const string& s) {
+  return get_suffix_array2(s);
+}
+
+vector<size_t> get_lcp_array_brut(const string& s, const vector<int>& sa) {
   vector<size_t> result;
   for (size_t i=0; i < sa.size() - 1; ++i) {
     size_t lcp = 0;
@@ -290,22 +163,20 @@ vector<size_t> get_lcp_array(const string& s, const vector<size_t>& sa) {
   return result;
 }
 
-vector<size_t> get_lcp_array2(const string& s, const vector<int>& sa)
-{
-    int n=s.size(),k=0;
-    vector<size_t> lcp(n,0);
-    vector<size_t> rank(n,0);
+vector<size_t> get_lcp_array(const string& s, const vector<int>& sa) {
+    size_t n = s.size();
+    vector<size_t> rank(n);
+    for (size_t i=0; i < n; ++i) { rank[sa[i]] = i; }
 
-    for(int i=0; i<n; i++) rank[sa[i]]=i;
-
-    for(int i=0; i<n; i++, k?k--:0)
-    {
-        if(rank[i]==n-1) {k=0; continue;}
-        int j=sa[rank[i]+1];
-        while(i+k<n && j+k<n && s[i+k]==s[j+k]) k++;
-        lcp[rank[i]]=k;
+    vector<size_t> lcp(n - 1);
+    size_t k = 0;
+    for (size_t i=0; i < n; ++i) {
+      if (rank[i] == 0) { continue; }
+      auto iprim = sa[rank[i] - 1];
+      while (s[i + k] == s[iprim + k]) { ++k; }
+      lcp[rank[i] - 1] = k;
+      if (k) { --k; }
     }
-    lcp.pop_back();
     return lcp;
 }
 
@@ -315,39 +186,26 @@ int main() {
 
   string s1, s2, s3;
   cin >> s1 >> s2 >> s3;
-
   size_t min_size = min({s1.size(), s2.size(), s3.size()});
-  
   string s = s1 + "1" + s2 + "2" + s3 + "3";
-  //string s;
-  //cin >> s;
-  /*
-  auto st = SuffixTree(vector<int>(s.begin(), s.end()), 'z', {s1.size(), s1.size() + 1 + s2.size(), s1.size() + 1 + s2.size() + 1 + s3.size()});
 
-  st.solve();
-  */
-
-
-  auto sa = get_suffix_array2(s);
+  auto sa = get_suffix_array(s);
   
-  /*
-  auto saMine = get_suffix_array(s);
-  println(sa, saMine);
-  cout << endl;
+  auto saMine = get_suffix_array_brut(s);
+  //println(sa, saMine);
+  //cout << endl;
   assert(saMine.size() == s.size());
   assert(sa.size() == s.size());
   for (int i=0; i < sa.size(); ++i) {
     assert (sa[i] == saMine[i]);
   }
-  */
+  
+  auto lcp = get_lcp_array(s, sa);
 
-
-  auto lcp = get_lcp_array2(s, sa);
-
-  /*
-  auto lcpMine = get_lcp_array(s, sa);
-  println(lcp, lcpMine);
-  cout << endl;
+  /* 
+  auto lcpMine = get_lcp_array_brut(s, sa);
+  //println(lcp, lcpMine);
+  //cout << endl;
   assert(lcpMine.size() == sa.size() - 1);
   assert(lcp.size() == sa.size() - 1);
   assert (lcpMine.size() == lcp.size());
